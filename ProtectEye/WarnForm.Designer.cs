f987bb450cf9492a053fa119d193e3c265eadea3
tanhd <1137146278@qq.com>
2019-01-06 22:48:22 +0800

    添加休息提醒的窗口,完成服务定时运行

---
+++ ProtectEye/WarnForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ProtectEyeService
+{
+    partial class WarnForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelTimeLeft = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStartWork = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(277, 135);
+            this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "注意休息!";
+            // 
+            // labelTimeLeft
+            // 
+            this.labelTimeLeft.AutoSize = true;
+            this.labelTimeLeft.Location = new System.Drawing.Point(373, 227);
+            this.labelTimeLeft.Name = "labelTimeLeft";
+            this.labelTimeLeft.Size = new System.Drawing.Size(26, 27);
+            this.labelTimeLeft.TabIndex = 1;
+            this.labelTimeLeft.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(204, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "剩余时间:";
+            // 
+            // btnStartWork
+            // 
+            this.btnStartWork.Location = new System.Drawing.Point(284, 290);
+            this.btnStartWork.Name = "btnStartWork";
+            this.btnStartWork.Size = new System.Drawing.Size(174, 56);
+            this.btnStartWork.TabIndex = 2;
+            this.btnStartWork.Text = "开始工作";
+            this.btnStartWork.UseVisualStyleBackColor = true;
+            // 
+            // WarnForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(822, 553);
+            this.Controls.Add(this.btnStartWork);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelTimeLeft);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("宋体", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Name = "WarnForm";
+            this.Text = "护眼提示";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelTimeLeft;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStartWork;
+    }
+}
